67ac84cf4adde38d55eeaf2ef0da4187ae4e320b
priyanka yadav <py631588@gmail.com>
2021-01-07 08:34:19 +0530

    componentDidMount

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+//import area
+//import something from 'somelibrary';
+
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Create an class Component
+class Oklabs extends Component{
+    //1.Property
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //2.Constructor
+    constructor(){
+        super();
+        this.state = { name : 'Mahima'};
+    }
+
+    //3.Method
+    //Every Class Component Must have render method
+    render(){
+        return(
+            <div>
+                <h1>Hello { this.state.name }</h1>
+            </div>
+        );
+    }
+    //This method calls after the view is rendered
+    componentDidMount(){
+       setTimeout(() => {
+        this.setState({name: 'Priyanka'})
+       },5000);
+    }
+}
+//Object.method();
+ReactDOM.render(<Oklabs/>,document.getElementById('root'));//actual argument
